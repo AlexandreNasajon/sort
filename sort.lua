1105a05d44ae699dbd06064177de8ab0669b7767
nasajon-chan <alexandrenasajon66@hotmail.com>
2017-06-10 22:14:10 -0300

    Update sort.lua

--- sort.lua
+++ sort.lua
@@ -1,12 +1,21 @@
-a = {5,7,6,3,4}
-
-sort = function(t)
-  for k,v in pairs(t) do
-      if k > k+1 and v < v+1 then
-      v = v+1
-  end
+sort1 = function(a,b)
+b[1] = a[1] -- pega o primeiro elemento de A e coloca em B
+n = 101*#a
+while n > 0 do
+    for i = 1,#a do -- vê todos os elementos de A
+        
+        if a[i+1] ~= nil and b[i] > a[i+1] then -- se o elemento i de B for maior que o elemento i+1 de A,
+            
+            b[i+1] = b[i] -- o elemento i+1 de B se torna uma cópia do elemento i de B
+        
+            b[i] = a[i+1] -- o elemento i de B se torna uma cópia do elemento i+1 de A
+            
+        else
+            b[i+1] = a[i+1] -- o elemento i+1 de B se torna uma cópia do elemento i+1 de A
+        end
+        
+    end
+    n = n-1
 end
-  
-b = sort(a)
-  
-print(b)
+    return b
+end
