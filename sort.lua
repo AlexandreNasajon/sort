2d8a8de2e5746febd0e264a217e3a68a57b7cc74
nasajon-chan <alexandrenasajon66@hotmail.com>
2018-08-20 13:30:16 +0000

    Update sort.lua

--- sort.lua
+++ sort.lua
@@ -1,5 +1,7 @@
 sort1 = function(a,b)
-b[1] = a[1] -- pega o primeiro elemento de A e coloca em B
+    
+    b[1] = a[1] -- pega o primeiro elemento de A e coloca em B
+    
     for i = 1,#a-1 do -- vê todos os elementos de A
         
         if b[i] > a[i+1] then -- se o elemento i de B for maior que o elemento i+1 de A,
@@ -9,10 +11,13 @@ b[1] = a[1] -- pega o primeiro elemento de A e coloca em B
             b[i] = a[i+1] -- o elemento i de B se torna uma cópia do elemento i+1 de A
             
         else
+            
             b[i+1] = a[i+1] -- o elemento i+1 de B se torna uma cópia do elemento i+1 de A
+        
         end
         
     end
-end
+    
     return b
+
 end
