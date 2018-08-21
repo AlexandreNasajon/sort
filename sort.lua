77590635f54cc028c8f8b7ba8fe08245e757263c
nasajon-chan <alexandrenasajon66@hotmail.com>
2018-08-21 12:20:23 +0000

    Update sort.lua

--- sort.lua
+++ sort.lua
@@ -1,23 +1,23 @@
-sort1 = function(a,b)
-    
-    b[1] = a[1] -- pega o primeiro elemento de A e coloca em B
-    
-    for i = 1,#a-1 do -- vê todos os elementos de A
-        
-        if b[i] > a[i+1] then -- se o elemento i de B for maior que o elemento i+1 de A,
-            
-            b[i+1] = b[i] -- o elemento i+1 de B se torna uma cópia do elemento i de B
-        
-            b[i] = a[i+1] -- o elemento i de B se torna uma cópia do elemento i+1 de A
-            
-        else
-            
-            b[i+1] = a[i+1] -- o elemento i+1 de B se torna uma cópia do elemento i+1 de A
-        
-        end
-        
-    end
-    
-    return b
+t = { 3 , 2 , 1 , 2 , 5 , 8 , 10}
+n = {}
 
+sort = function(table,new)
+
+    for k , v in pairs(table) do
+        new[k] = table[k]
+    end
+
+    for i = 1 , #new do
+
+        for j = 1 , #new do
+
+            if new[i] > new[j] then
+                new[i] , new[j] = new[j] , new[i]
+            end
+        end
+    end
 end
+
+sort(t,n)
+
+for i = 1 , #n do print(n[i]) end
